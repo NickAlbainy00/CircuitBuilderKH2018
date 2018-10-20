36a93ddce9ec3589ec6b59bfbc2a078eaf4a41e8
Jonathan Bender <jonathon.bender@rockets.utoledo.edu>
2018-10-20 02:50:42 -0400

    Made ILogicNode, ILogicGate

---
+++ CircuitBuilder/Logic/ILogicGate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CircuitBuilder.Logic
+{
+    public interface ILogicGate
+    {
+        // Returns the number of inputs for the given gate
+        int GetInputSize();
+        // See above for outputs.
+        int GetOutputSize();
+        // Returns the logical expression as a boolean for the given node.
+        string Evaluate();
+    }
+}
