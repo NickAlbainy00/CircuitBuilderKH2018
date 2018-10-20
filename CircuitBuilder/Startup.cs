266ee8294964013c81d6da80b695bc1292f69a46
NickAlbainy00 <NickAlbainy00@users.noreply.github.com>
2018-10-19 23:17:03 -0400

    Initial Commit
    GH Setup

---
+++ CircuitBuilder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CircuitBuilder.Startup))]
+namespace CircuitBuilder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
