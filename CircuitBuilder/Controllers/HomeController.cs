48daae3e7564ebaf6e6b4eaf8a6a3312f413c6ad
mrjoeboo123 <joey.a.kilgore@gmail.com>
2018-10-20 13:16:15 -0400

    Circuit Checker
    checks if the circuit has valid connections that end with sources

--- CircuitBuilder/Controllers/HomeController.cs
+++ CircuitBuilder/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace CircuitBuilder.Controllers
 {
     public class HomeController : Controller
     {
+
+
+
+
         public ActionResult Index()
         {
             return View();
---
+++ CircuitBuilder/Logic/CircuitChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CircuitBuilder.Logic
+{
+    public static class CircuitChecker
+    {
+        // Circuit checker is used to check whether a circuit works
+
+        /// <summary>
+        /// Checks if the circuit has a valid set of connections from output to input
+        /// </summary>
+        /// <returns></returns>
+        public static bool isValidCircuit(ILogicNode node)
+        {
+            // if the node is a gate, check that there are no inputs set to null 
+            if (node.IsGate())
+            {
+               foreach(ILogicNode child in node.GetNodeInputs()){
+                    // 
+
+                    if(child == null)
+                    {
+                        return false;
+                    }else if (child.IsGate())
+                    {
+                        isValidCircuit(child);
+                    }
+               }
+            }
+
+            // if not a gate, then it is an input and the connection is valid
+            return true;
+        }
+    }
+}
